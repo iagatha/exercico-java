29ff99c419eb8f43cbf7b7d91f6bb1b62a0cf049
iagatha <iagathasouzaa@gmail.com>
2023-04-22 20:11:07 -0300

    exe

---
+++ src/ImpostoRenda.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class ImpostoRenda {
+
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+
+        double salario;
+        double imposto = 0;
+        double diferenca;
+
+        System.out.println("Digite seu salario: ");
+        salario = leitor.nextDouble();
+
+        if(salario > 4500){
+            imposto = 1000 * 0.08 + 1500 * 0.18; 
+            diferenca = salario - 4500;
+            imposto += diferenca * 0.28;
+        }else if(salario > 3000){
+            imposto = 1000 * 0.08;
+            diferenca = salario - 3000;
+            imposto += diferenca * 0.18;
+        }else if(salario > 2000){
+            diferenca = salario - 2000;
+            imposto = diferenca * 0.08;
+        }
+        if(imposto > 0){
+            System.out.println("R$ " + imposto);
+        }else{
+            System.out.println("ISENTO");
+        }
+
+        
+    }
+}
